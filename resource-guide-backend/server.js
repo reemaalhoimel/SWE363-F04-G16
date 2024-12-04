b079d399c7300958ef9e76a40e404e0b698022bf
Danah Aljameel <144171891+Dana16v@users.noreply.github.com>
2024-12-04 17:36:55 +0300

    Add files via upload

---
+++ resource-guide-backend/server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+/*const bodyParser = require("body-parser");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");*/
+
+/*dotenv.config();
+connectDB();*/
+
+const app = express();
+
+app.use(express.json()); // For parsing JSON requests
+/*app.use(bodyParser.urlencoded({ extended: false }));*/
+app.use(express.static("../front-end"));
+
+app.use("/api/admins", require("./api/admins"));
+app.use("/api/bans", require("./api/bans"));
+app.use("/api/comments", require("./api/comments"));
+app.use("/api/courses", require("./api/courses"));
+app.use("/api/moderators", require("./api/moderators"));
+app.use("/api/posts", require("./api/posts"));
+app.use("/api/users", require("./api/users"));
+
+app.listen(3000);
